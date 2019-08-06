373b6c413148ee863997c8a177c4e14800f21946
MikeCheng1208 <a3804430@hotmail.com>
2019-08-07 00:04:46 +0800

    keyboard

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ const gameStart = {
         this.load.image('bg4', 'images/bg/bg4.png');
         this.load.image('footer', 'images/bg/footer.png');
         this.load.image('rock1', 'images/item-level-1-rock.png');
+        this.load.spritesheet('user', 'images/player.png', {frameWidth: 144, frameHeight: 120});
     },
     create: function(){
         this.bg4 = this.add.tileSprite(400, 180, 800, 360, 'bg4');
@@ -17,21 +18,77 @@ const gameStart = {
         this.bg1 = this.add.tileSprite(400, 180, 800, 360, 'bg1');
         this.footer = this.add.tileSprite(400, 310, 800, 90, 'footer');
         this.rock1 = this.add.tileSprite(cw, 215, 200, 104, 'rock1');
+        // this.rock1.setScale(0.6);
 
-        this.physics.add.existing(this.footer);
-        this.footer.body.immovable = true;
-        this.footer.body.moves = false;
+        //設定人物位置與加入物理效果
+        this.player = this.physics.add.sprite(150, 50, 'user');
+        this.player.setCollideWorldBounds(true); //角色邊界限制
+        this.player.setBounce(0.2); //設定彈跳值
+        this.player.setScale(0.6); //設定顯示大小
+        this.player.setSize(110, 120, 0); //碰撞邊界
 
-        this.physics.add.existing(this.rock1);
-        this.rock1.body.immovable = true;
-        this.rock1.body.moves = false;
+        this.anims.create({
+            key: 'run',
+            frames: this.anims.generateFrameNumbers('user', { start: 0, end: 1 }),
+            frameRate: 5,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'jump',
+            frames: this.anims.generateFrameNumbers('user', { start: 2, end: 3 }),
+            frameRate: 5,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'speed',
+            frames: this.anims.generateFrameNumbers('user', { start: 4, end: 5 }),
+            frameRate: 5,
+            repeat: -1
+        })
+
+        const addPhysics = GameObject =>{
+            this.physics.add.existing(GameObject);
+            GameObject.body.immovable = true;
+            GameObject.body.moves = false;
+        }
+
+        addPhysics(this.footer);
+        addPhysics(this.rock1);
+
+        this.physics.add.collider(this.player, this.footer);
+        this.physics.add.collider(this.player, this.rock1);
+
+        //播放動畫
+        this.player.anims.play('run', true);
+        console.log(this.player);
+        
     },
     update: function(){
         this.bg3.tilePositionX += 1;
         this.bg2.tilePositionX += 2;
         this.bg1.tilePositionX += 3;
         this.footer.tilePositionX += 3;
-        this.rock1.x -= 3;
+        // this.rock1.x -= 3;
+
+         // 啟動鍵盤事件
+         let cursors = this.input.keyboard.createCursorKeys();
+         if (cursors.right.isDown) {
+             this.player.setVelocityX(200);
+             this.player.flipX = false;
+             this.player.setSize(144, 120, 0); //碰撞邊界
+             this.player.anims.play('speed', true);
+         } else if (cursors.left.isDown) {
+             this.player.setVelocityX(-260);
+             this.player.setSize(144, 120, 0); //碰撞邊界
+             this.player.anims.play('speed', true);
+         } else {
+            this.player.setVelocityX(0);
+            this.player.setSize(110, 120, 0); //碰撞邊界
+            this.player.anims.play('run', true);
+         }
+         if (cursors.up.isDown && this.player.body.touching.down) {
+             this.player.setVelocityY(-700);
+         }
     }
 }
 const config = {
@@ -45,7 +102,7 @@ const config = {
             gravity: {
                 y: 1500
             },
-            debug: true,
+            // debug: true,
         },
     },
     scene: [
