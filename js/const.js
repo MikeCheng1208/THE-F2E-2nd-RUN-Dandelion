97c90b2c734a6d16050a9f1679fb3e82c007888f
MikeCheng1208 <a3804430@hotmail.com>
2019-08-07 22:10:59 +0800

    add scene

---
+++ js/const.js
@@ -0,0 +1,4 @@
+const cw = 800;
+const ch = 450;
+const scale = 0.6;
+const monsterScale = 0.7;
---
+++ js/gameStart.js
@@ -0,0 +1,13 @@
+const gameStart = {
+    key: 'gameStart',
+    preload: function(){
+        
+    },
+    create: function(){
+        
+        
+    },
+    update: function(){
+        
+    }
+}
